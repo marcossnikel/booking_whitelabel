ddbc67157754ed302eb0774a59bd4a3ff4bd5a42
marcossnikel <marcosnikeldev@gmail.com>
2024-07-10 15:37:24 -0300

    initial setup config

---
+++ src/main/java/booking_white_label/example/demo/BookingWhiteLabelApplication.java
@@ -0,0 +1,13 @@
+package booking_white_label.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingWhiteLabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingWhiteLabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/booking_white_label/example/demo/BookingWhiteLabelApplicationTests.java
@@ -0,0 +1,13 @@
+package booking_white_label.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingWhiteLabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
